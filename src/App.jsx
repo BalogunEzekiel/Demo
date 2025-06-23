946ca436aa60fcf387225eac00dba11a03ee82fc
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-06-23 13:16:16 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HelloWorld from './components/HelloWorld';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to My Vite + React App!</h1>
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
